2802b5a54311a2878325824d0da361874800dbb8
Duong The <nopbongdem@gmail.com>
2016-07-19 20:57:05 +0700

    change facades

--- src/Support/Facades/QsoftCache.php
+++ src/Support/Facades/QsoftCache.php
@@ -3,7 +3,7 @@
  * @Author: Duong The
  * @Date:   2016-07-15 16:04:37
  * @Last Modified by:   Duong The
- * @Last Modified time: 2016-07-15 19:09:53
+ * @Last Modified time: 2016-07-19 20:56:22
  */
 namespace Qsoft\Seo\Support\Facades;
 
@@ -13,6 +13,6 @@ class QsoftCache extends Facade
 {
     protected static function getFacadeAccessor()
     {
-        return 'qsoft_cache';
+        return 'QsoftCache';
     }
 }
--- src/Support/Facades/QsoftClawer.php
+++ src/Support/Facades/QsoftClawer.php
@@ -3,7 +3,7 @@
  * @Author: Duong The
  * @Date:   2016-07-15 16:04:37
  * @Last Modified by:   Duong The
- * @Last Modified time: 2016-07-15 16:08:49
+ * @Last Modified time: 2016-07-19 20:56:28
  */
 namespace Qsoft\Seo\Support\Facades;
 
@@ -13,6 +13,6 @@ class QsoftClawer extends Facade
 {
     protected static function getFacadeAccessor()
     {
-        return 'qsoft_clawer';
+        return 'QsoftClawer';
     }
 }
